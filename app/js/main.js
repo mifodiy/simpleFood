ca62d25b654ba1243e48ba8be1fae77d2a3d3c7f
mifodiy <mifodiy911@gmail.com>
2022-10-22 14:14:31 +0300

    added review and mobile section

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,9 @@
 $(function () {
-
+  $('.reviews__slider').slick({
+    dots:true,
+    prevArrow: `<button type="button" class="slick-prev"><span class="icon-arrow" style="-webkit-mask-image: url('../images/sprite.svg#left-arrow');"></span></button>`,
+    nextArrow: `<button type="button" class="slick-next"><span class="icon-arrow" style="-webkit-mask-image: url('../images/sprite.svg#right-arrow');"></span></button>`,
+  })
 
   var mixer = mixitup('.categories__list');
   mixer.filter('.burger');
